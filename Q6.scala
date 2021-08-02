21ef9a9a0d80e6c522d978b73141b96d9405d855
Prasadkpd <2019cs078@stu.ucsc.lk>
2021-08-02 21:58:50 +0530

    first commit

--- Q6.scala
+++ Q6.scala
@@ -2,7 +2,6 @@
 //Q6
 //The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . 
 
-Each subsequent number is the sum of the previous two.
 import scala.io.StdIn.readInt;
 
 object Q6{
